e2c2cf9d8bc172dc39effa0950252f240a61d1ff
Rumana S <84440177+Rumana20@users.noreply.github.com>
2021-12-04 14:20:23 +0530

    Update Fumble.py

--- Fumble.py
+++ Fumble.py
@@ -4,7 +4,7 @@ import pandas as pd
 import pickle
 from io import StringIO
 
-st.title("Home")
+
 
 def main():
    
@@ -25,9 +25,9 @@ import streamlit as st
 
 st.markdown('##')
 
-okc = pd.read_excel("D:/5th Sem/Project/Copy of User Details_Faf.xlsx",engine=(engine)
+okc = pd.read_excel("D:/5th Sem/Project/Copy of User Details_Faf.xlsx",engine=(engine))
 
-
+st.title("Home")
 if __name__=='__main__':
     main() 
     
