088ab3271552bae597c807aff0f4382f0aa1b467
Rumana S <84440177+Rumana20@users.noreply.github.com>
2021-12-04 14:22:24 +0530

    Update Fumble.py

--- Fumble.py
+++ Fumble.py
@@ -25,7 +25,7 @@ import streamlit as st
 
 st.markdown('##')
 
-okc = pd.read_excel("D:/5th Sem/Project/Copy of User Details_Faf.xlsx",engine=(engine))
+okc = pd.read_excel("D:/5th Sem/Project/Copy of User Details_Faf.xlsx",engine=("xlrd"))
 
 st.title("Home")
 if __name__=='__main__':
