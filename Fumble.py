b3ad0d5ecab75d0121114952d50a625de0591ec3
Rumana S <84440177+Rumana20@users.noreply.github.com>
2021-12-15 22:46:44 +0530

    Update Fumble.py

--- Fumble.py
+++ Fumble.py
@@ -32,7 +32,7 @@ st.set_page_config(
 okc = pd.read_excel("Copy of User Details_Faf.xlsx")
 
 #st.title("Infinity")
-st.image("image.png",width=300)
+st.image("image.png",width=250)
 
 st.markdown('##')  
 
